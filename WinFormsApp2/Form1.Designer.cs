405059eb1e1904912b0c269f69267d08f874e436
Gutes Tod <jefflol003@gmail.com>
2023-02-26 20:39:56 +0500

    Добавление TabControl и Buttons

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -20,27 +20,10 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
-        }
-
-        #endregion
+        private TabControl tabControl1;
+        private TabPage tab_page1;
+        private TabPage tab_page2;
+        private Button btn_prev;
+        private Button btn_next;
     }
 }
--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -6,5 +6,81 @@ namespace WinFormsApp2
         {
             InitializeComponent();
         }
+
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tab_page1 = new System.Windows.Forms.TabPage();
+            this.tab_page2 = new System.Windows.Forms.TabPage();
+            this.btn_prev = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tab_page1);
+            this.tabControl1.Controls.Add(this.tab_page2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(300, 176);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tab_page1
+            // 
+            this.tab_page1.Location = new System.Drawing.Point(4, 24);
+            this.tab_page1.Name = "tab_page1";
+            this.tab_page1.Padding = new System.Windows.Forms.Padding(3);
+            this.tab_page1.Size = new System.Drawing.Size(292, 148);
+            this.tab_page1.TabIndex = 0;
+            this.tab_page1.Text = "Page 1";
+            this.tab_page1.UseVisualStyleBackColor = true;
+            // 
+            // tab_page2
+            // 
+            this.tab_page2.Location = new System.Drawing.Point(4, 24);
+            this.tab_page2.Name = "tab_page2";
+            this.tab_page2.Padding = new System.Windows.Forms.Padding(3);
+            this.tab_page2.Size = new System.Drawing.Size(292, 148);
+            this.tab_page2.TabIndex = 1;
+            this.tab_page2.Text = "Page 2";
+            this.tab_page2.UseVisualStyleBackColor = true;
+            // 
+            // btn_prev
+            // 
+            this.btn_prev.Location = new System.Drawing.Point(4, 178);
+            this.btn_prev.Name = "btn_prev";
+            this.btn_prev.Size = new System.Drawing.Size(75, 23);
+            this.btn_prev.TabIndex = 0;
+            this.btn_prev.Text = "Prev";
+            this.btn_prev.UseVisualStyleBackColor = true;
+            // 
+            // btn_next
+            // 
+            this.btn_next.Location = new System.Drawing.Point(225, 178);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(75, 23);
+            this.btn_next.TabIndex = 1;
+            this.btn_next.Text = "Next";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(873, 417);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.btn_prev);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
